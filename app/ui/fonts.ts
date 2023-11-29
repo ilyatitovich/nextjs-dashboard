27e48fdf65a7ee06eaa4be56d87a1832294f1a85
ilyatitovich <ilyatitovich@gmail.com>
2023-11-29 13:53:05 +0100

    connect vercel postgres

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+// keep all fonts here
+
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"]});
